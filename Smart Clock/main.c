8ec3d964fd521556d3638d871245687ab33b194c
Unknown <mohamed_medhat1996@yahoo.com>
2018-06-02 09:15:15 +0200

    Intterrupt Vectors

--- Smart Clock/main.c
+++ Smart Clock/main.c
@@ -2,118 +2,221 @@
  * Smart Clock.c
  *
  * Created: 6/1/2018 7:42:36 PM
- * Author : Mohamed Medhat
- */ 
-
+ */
+#define timer_vect TIMER1_COMPA_vect
+#define mode_bit
+#define mode_port
+#define mode_vect PCINT0_vect
+#define temp_bit
+#define temp_port
+#define toggle_bit
+#define toggle_port
+#define toggle_vect PCINT1_vect
+#define ok_bit
+#define ok_port
+#define ok_vect PCINT2_vect
+#define up_bit
+#define up_port
+#define up_vect PCINT3_vect
+#define down_bit
+#define down_port
+#define down_vect PCINT3_vect
+#define LCD_port
 #include <avr/io.h>
-#include <util/delay.h>
-#include <avr/interrupt.h>
-#include "LCD.h"
 
-#define SET_BIT(ADDRESS, BIT) ADDRESS |= (1<<BIT)
-#define RESET_BIT(ADDRESS, BIT) ADDRESS &= ~(1<<BIT)
-#define TOGGLE_BIT(ADDRESS, BIT) ADDRESS ^= (1<<BIT)
-#define READ_BIT(ADDRESS, BIT) ((ADDRESS & (1<<BIT))>>BIT)
+unsigned short mode = 0;
+unsigned short n_modes = 2;
 
-/*Global Variables Declarations*/
-unsigned char HOURS = 0;
-unsigned char MINUTES = 0;
-unsigned char SECONDS = 0;
+unsigned short n_toggles = 0;
+unsigned short toggle_count = 0;
 
-unsigned char NewHours;
-unsigned char NewMinutes;
-unsigned char NewSeconds;
+unsigned short temperature_c = 0;
+
+unsigned short hour = 1;
+unsigned short minute = 0;
+unsigned short second = 0;
 
 
-void CTC_mode ()
-{
-	// Set the Timer Mode to CTC
-	TCCR0A |= (1 << WGM01);
+unsigned short days = 0;
+unsigned short day = 1;
+unsigned short year = 1;
+unsigned short month = 1;
 
-	// Set the value that you want to count to
-	OCR0A = 0x3c;
-	
-	// start the timer (No Prescalling)
-	TCCR0B |= (1 << CS00);
-	
-	TIMSK0 |= (1 << OCIE0A);    //Set the ISR COMPA vector
+unsigned long alarm_count = 0;
+unsigned long alarm_value = 0;
+unsigned short alarm_state = 0;
+unsigned short fire_alarm = 0;
+unsigned short alarm_hour = 0;
+unsigned short alarm_minute = 0;
+unsigned short alarm_second = 0;
 
-	sei();         //enable interrupts
+void AdjustAlarm(){
+    alarm_state = 1;
+    alarm_hour = (alarm_value/60*60)%13;
+    alarm_minute = (alarm_value/60)%61;
+    alarm_second = (alarm_value)%61;
 }
 
-void Increase (int data)
-{
-	if(data<59) data++;
-	else data=0;
+void CancelAlarm(){
+    alarm_state = 0;
+    alarm_count = 0;
 }
 
-void Decrease (int data)
-{
-	if(data>0) data--;
-	else data=60;
+void UpdateTime(){
+    time = time%(24*60*60);
+    hour = (time/60*60)%12 + 1;
+    minute = (time/60)%61;
+    second = time%61;
 }
 
-void Done (int )
-{
-	
+void UpdateDate(){
+    days = (days + time/(24*60*60-1))%361;
+    year = days/361;
+    month = (days/13)%31;
+    day = days%31;
+}
+
+void UpdateTemperature(){
+    temperature_c = adc_read(&temp_port, temp_bit);
+}
+
+void UpdateLCD(){
+    if(mode == 0){
+        //display year,month,day,temperature_c,alarm_state,hour,minute,second,am/pm
+    }
+    else if(mode == 1){
+        //display alarm_hour,alarm_minute,alarm_second,temperature_c
+    }
+}
+
+ISR(mode_vect){
+    //mode button is clicked
+    _delay_ms(500);
+    mode = (mode+1)%n_modes;
+    toggle_count = 0;
+}
+
+ISR(toggle_vect){
+    //toggle button is clicked
+    _delay_ms(500);
+    switch(mode){
+        case 0 : n_toggles = 8;
+        case 1 : n_toggles = 6;
+    }
+
+    toggle_count = (toggle_count+1)%n_toggles;
+}
+
+ISR(up_vect){
+    //up button is clicked
+    _delay_ms(500);
+    if(mode == 0 ){
+        switch(toggle_count){
+            case 0 : ;
+            case 1 : time += 60*60;
+            case 2 : time += 60;
+            case 3 : time++;
+            case 4 : pm/am
+            case 5 : days += 1;
+            case 6 : days += 30;
+            case 7 : days += 360;
+        }
+        UpdateDate();
+        UpdateTime();
+    }
+    else if(mode == 1){//alarm mode
+        switch(toggle_count){
+            case 0 : ;
+            case 1 : alarm_value += 60*60;
+            case 2 : alarm_value += 60;
+            case 3 : alarm_value++;
+            case 4 : pm/am
+            case 5 : toggle(alarm_state);
+        }
+        if(alarm_state == 0)
+            CancelAlarm();
+        else
+            AdjustAlarm();
+    }
+    UpdateLCD();
+}
+
+ISR(down_vect){
+    //down button is clicked
+    _delay_ms(500);
+    if(mode == 0 ){
+        switch(toggle_count){
+            case 0 : ;
+            case 1 : time -= 60*60;
+            case 2 : time -= 60;
+            case 3 : time--;
+            case 4 : pm/am
+            case 5 : days -= 1;
+            case 6 : days -= 30;
+            case 7 : days -= 360;
+        }
+        UpdateDate();
+        UpdateTime();
+    }
+    else if(mode == 1){//alarm mode
+        switch(toggle_count){
+            case 0 : ;
+            case 1 : alarm_value -= 60*60;
+            case 2 : alarm_value -= 60;
+            case 3 : alarm_value--;
+            case 4 : pm/am
+            case 5 : toggle(alarm_state);
+        }
+        if(alarm_state == 0)
+            CancelAlarm();
+        else
+            AdjustAlarm();
+    }
+    UpdateLCD();
+}
+
+ISR(ok_vect){
+    //ok button is clicked
+    _delay_ms(500);
+    switch(mode){
+        case 0 : toggle_count = 0;
+        case 1 : mode = 0;
+    }
+    UpdateLCD();
+}
+
+ISR(timer_vect){
+    //every 1 second
+    time++;
+    UpdateDate();
+    UpdateTime();
+    UpdateTemperature();
+    //check for the alarm
+    if(alarm_state == 1 && alarm_value == alarm_count)
+        fire_alarm = 1;
+    else if(alarm_state == 1)
+        alarm_count++;
+
+    //check for mode
+    if(mode == 0){
+        //clock mode
+        //update LCD
+        LCD_SendString();
+    }
+    else if(mode == 1){
+        //alarm mode
+        //update LCD
+        LCD_SendString();
+    }
 }
 
 int main(void)
 {
-	char mode;
-	char toggle;
-	char up;
-	char down;
-	char ok;
-	CTC_mode();
-    while (1) 
-    {
-		mode=PINC; //Default in Clock mode
-		READ_BIT(mode,0);
-		toggle=PINC;
-		READ_BIT(toggle,1);
-		up=PINC;
-		READ_BIT(up,2);
-		down=PINC;
-		READ_BIT(down,3);
-		ok=PINC;
-		READ_BIT(ok,4);
-		unsigned char data;
-		
-		if(mode==0)
-		{
-			//change from mode to anther
-		}
-		if(toggle==0)
-		{
-			//Select (hours or minutes or seconds)
-			// data will be an (hours or minutes or seconds)
-		}
-		if(up==0) Increase(data);
-		if(down==0) Decrease(data);
-		if(ok == 0) Done();
-		
-		//display time
-    }
-}
 
-ISR (TIMER0_COMPA_vect)  // timer0 overflow interrupt
-{
-	SECONDS++;
-	
-	if(SECONDS==60)
-	{
-		SECONDS=0;
-		MINUTES++;	
-	}
-	
-	if(MINUTES==60)
-	{
-		MINUTES=0;
-		HOURS++;
-	}
-	
-	if(HOURS>23) HOURS=0;
-	
-	//display time
+    // Insert code
+
+    while(1)
+    ;
+
+    return 0;
 }
